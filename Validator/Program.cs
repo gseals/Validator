c505715c2a8b45a86483765b3f8bfb524a080fc9
Gabriel Seals <sealsgj@gmail.com>
2020-03-05 21:26:00 -0600

    Add project files.

---
+++ Validator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a numeric pin between 4 and 8 characters");
+            var testPin = Console.ReadLine();
+
+            if (testPin.Length > 3 && testPin.Length < 9 && Regex.IsMatch(testPin, @"^\d+$"))
+            {
+                Console.WriteLine(true);
+                Console.ReadLine();
+            }
+            else if (testPin.Length <= 3 || testPin.Length >= 9 || Regex.IsMatch(testPin, @"^[A-Za-z]+$"))
+            {
+                Console.WriteLine("Something's not right");
+                Console.ReadLine();
+            }
+        }
+    }
+}
