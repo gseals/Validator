975ea95eafee9d4d6912854dc227a30c4befc350
Gabriel Seals <sealsgj@gmail.com>
2020-03-06 21:33:53 -0600

    changes

--- Validator/Program.cs
+++ Validator/Program.cs
@@ -21,6 +21,18 @@ namespace Validator
                 Console.WriteLine("Something's not right");
                 Console.ReadLine();
             }
+
+            //second test begins here
+            Console.WriteLine("Please enter a 10-digit phone number. You may include spaces, dashes, and parentheses.");
+            var phoneNumber = Console.ReadLine();
+
+            if (phoneNumber.Length < 11 && phoneNumber.Length > 0 && Regex.IsMatch(testPin, @"^\d+$") && phoneNumber.IndexOf("555") == -1) ;
+            {
+                Console.WriteLine("That's a fine looking phone number you got there.");
+                Console.ReadLine();
+            }
+            Console.WriteLine("what is the truth?" + phoneNumber.IndexOf("555"));
+            Console.ReadLine();
         }
     }
 }
